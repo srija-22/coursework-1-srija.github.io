fa42163c1fb8b6a58f6ab14909515bf422d29ef8
srija <srija5543@gmail.com>
2022-11-18 16:20:28 +0000

    add methods in main.js

--- main.js
+++ main.js
@@ -16,4 +16,37 @@ const app = new Vue({
         },
         lessons: lessons,
     },
+    methods: {
+        addToCart(lesson) {
+            this.cart.push(lesson);
+
+            this.lessons.find(function(item) {
+                if (item.id === lesson.id) lesson.spaces = --lesson.spaces
+            })
+        },
+        removeFromCart(id) {
+            const index = this.cart.findIndex((item) => item.id === id);
+
+            this.cart.splice(index, 1);
+
+            this.lessons.forEach(function(lesson) {
+                if (lesson.id === id) lesson.spaces += 1
+            })
+
+            if (this.cart.length == 0) this.showCart = false
+        },
+        toggleCart() {
+            this.showCart = !this.showCart
+        },
+        checkout() {
+            alert("You've checked out successfully");
+
+            this.cart = [];
+
+            this.name = ""
+            this.phone = ""
+
+            location.reload()
+        },
+    },
 });
