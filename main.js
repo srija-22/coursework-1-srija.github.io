06dfcb701aa515492e0140f964fcdc5b872f6188
srija <srija5543@gmail.com>
2022-11-18 16:20:53 +0000

    add computed and watch properties in main.js

--- main.js
+++ main.js
@@ -49,4 +49,132 @@ const app = new Vue({
             location.reload()
         },
     },
+    computed: {
+        cartSize: function () {
+            return this.cart.length;
+        },
+
+        filteredLessons() {
+            let modifiedLessons = this.lessons;
+
+            if (this.searchValue != "" && this.searchValue) {
+                modifiedLessons = this.lessons.filter((item) => {
+                    return item.subject.toUpperCase().includes(this.searchValue.toUpperCase()) || item.location.toUpperCase().includes(this.searchValue.toUpperCase());
+                });
+            }
+
+            if (this.orderBy === "asc") {
+
+                modifiedLessons = modifiedLessons.sort((a, b) => {
+                    if (this.sortBy == "subject") {
+
+                        if (a.subject.toLowerCase() < b.subject.toLowerCase()) {
+                            return -1;
+                        }
+                        if (a.subject.toLowerCase() > b.subject.toLowerCase()) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    } else if (this.sortBy == "location") {
+                        if (a.location.toLowerCase() < b.location.toLowerCase()) {
+                            return -1;
+                        }
+                        if (a.location.toLowerCase() > b.location.toLowerCase()) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    } else if (this.sortBy == "price") {
+                        if (a.price < b.price) {
+                            return -1;
+                        }
+                        if (a.price > b.price) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    } else if (this.sortBy == "availability") {
+                        if (a.spaces < b.spaces) {
+                            return -1;
+                        }
+                        if (a.spaces > b.spaces) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                });
+
+            } else {
+
+                modifiedLessons = modifiedLessons.sort((a, b) => {
+                    if (this.sortBy == "subject") {
+
+                        if (a.subject.toLowerCase() < b.subject.toLowerCase()) {
+                            return 1;
+                        }
+                        if (a.subject.toLowerCase() > b.subject.toLowerCase()) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    } else if (this.sortBy == "location") {
+                        if (a.location.toLowerCase() < b.location.toLowerCase()) {
+                            return 1;
+                        }
+                        if (a.location.toLowerCase() > b.location.toLowerCase()) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    } else if (this.sortBy == "price") {
+                        if (a.price < b.price) {
+                            return 1;
+                        }
+                        if (a.price > b.price) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    } else if (this.sortBy == "availability") {
+                        if (a.spaces < b.spaces) {
+                            return 1;
+                        }
+                        if (a.spaces > b.spaces) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                });
+
+            }
+
+            return modifiedLessons;
+        },
+        isFormValid() {
+            return this.name && this.phone && !this.nameError && !this.phoneError
+        }
+    },
+    watch: {
+        name(value) {
+            if (!value) {
+                this.nameError = "Please enter your name";
+            } else if (!/^[A-Za-z\s]*$/.test(value)) {
+                this.nameError = "Your name must only contain letters";
+            } else {
+                this.nameError = "";
+            }
+        },
+        phone(value) {
+            if (!value) {
+                this.phoneError = "Please enter your phone number";
+            } else if (!/^[0-9]+$/.test(value)) {
+                this.phoneError = "Your phone number must only contain numbers";
+            } else {
+                this.phoneError = "";
+            }
+        },
+    },
 });
