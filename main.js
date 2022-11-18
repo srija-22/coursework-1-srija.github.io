a55e31b69cd3f2e66b3032b748e92212d3e37a32
srija <srija5543@gmail.com>
2022-11-18 16:19:57 +0000

    add data properties in main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const app = new Vue({
     el: "#instance",
+    data: {
+        showCart: false,
+        searchValue: "",
+        name: "",
+        phone: "",
+        nameError: "",
+        phoneError: "",
+        sortBy: "",
+        orderBy: "",
+        cart: [],
+        error: {
+            name: "",
+            phone: "",
+        },
+        lessons: lessons,
+    },
 });
