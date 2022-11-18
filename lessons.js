7586bb831afe096aae03661b9471a0a0f0cfe83f
srija <srija5543@gmail.com>
2022-11-18 16:17:02 +0000

    add lessons in lessons.js

---
+++ lessons.js
@@ -0,0 +1,82 @@
+var lessons =  [
+    {
+      id: 1,
+      subject: "Cybersecurity",
+      location: "India",
+      price: 300,
+      spaces: 5,
+      img: "imgs/1.jpg"
+    },
+    {
+      id: 2,
+      subject: "Testing and Deployment",
+      location: "Texas",
+      price: 115,
+      spaces: 5,
+      img: "imgs/2.jpg"
+    },
+    {
+      id: 3,
+      subject: "Game Development",
+      location: "Kilburn",
+      price: 870,
+      spaces: 5,
+      img: "imgs/3.jpg"
+    },
+    {
+      id: 4,
+      subject: "Graphics Design",
+      location: "Las Vegas",
+      price: 90,
+      spaces: 5,
+      img: "imgs/4.jpg"
+    },
+    {
+      id: 5,
+      subject: "Business Operations and Intelligence",
+      location: "Real Madrid",
+      price: 35,
+      spaces: 5,
+      img: "imgs/5.jpg"
+    },
+    {
+      id: 6,
+      subject: "Novel Interactions",
+      location: "Canary Wharf",
+      price: 540,
+      spaces: 5,
+      img: "imgs/6.jpg"
+    },
+    {
+      id: 7,
+      subject: "Architecture",
+      location: "Tokyo",
+      price: 600,
+      spaces: 5,
+      img: "imgs/7.jpg"
+    },
+    {
+      id: 8,
+      subject: "Digital Media Technology",
+      location: "New York",
+      price: 130,
+      spaces: 5,
+      img: "imgs/8.jpg"
+    },
+    {
+      id: 9,
+      subject: "Technical Drawing",
+      location: "Berlin",
+      price: 230,
+      spaces: 5,
+      img: "imgs/9.jpg"
+    },
+    {
+      id: 10,
+      subject: "Medical Labs",
+      location: "Cabo",
+      price: 430,
+      spaces: 5,
+      img: "imgs/10.jpg"
+    },
+  ]
